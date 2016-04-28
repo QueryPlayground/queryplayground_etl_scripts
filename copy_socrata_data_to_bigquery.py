9719f6b3b7d309f1f76452840f3aff2ca451eac6
Tim Clemans <tim@insideyourgovernment.com>
2016-04-28 16:14:49 +0000

    debugging

--- copy_socrata_data_to_bigquery.py
+++ copy_socrata_data_to_bigquery.py
@@ -7,16 +7,17 @@ try:
     r.db('queryplayground').table_create('third_party_creds').run()
     socrata_app_token = raw_input('Your Socrata app token\n')
     r.db('queryplayground').table_create('third_party_creds').insert({"id": "socrata", "app_token": socrata_app_token}).run()
-    r.db('queryplayground').table('socrata_datasets').insert({"id": "data_seattle_gov_pu5n_trf4", "domain": "data.seattle.gov", "datasetid": "pu5n_trf4"}).run()
+    r.db('queryplayground').table('socrata_datasets').insert({"id": "data_seattle_gov_pu5n_trf4", "domain": "data.seattle.gov", "datasetid": "pu5n-trf4"}).run()
 except:
     pass
 import requests
+r.db('queryplayground').table('socrata_datasets').replace({"id": "data_seattle_gov_pu5n_trf4", "domain": "data.seattle.gov", "datasetid": "pu5n-trf4"}).run()
 for dataset in r.db('queryplayground').table('socrata_datasets').run():
     app_token = r.db('queryplayground').table('third_party_creds').get('socrata').run()['app_token']
     if not 'socrata_created_at' in dataset:
         local_filename = dataset['id']+'.csv'
         # NOTE the stream=True parameter
-        url = 'https://%s/resource/%s.csv?$limit=1000' % (dataset['domain'], dataset['datasetid'])
+        url = 'https://%s/resource/%s.csv?$select=:*,*&$limit=1' % (dataset['domain'], dataset['datasetid'])
         req = requests.get(url, stream=True)
         with open(local_filename, 'wb') as f:
             for chunk in req.iter_content(chunk_size=1024): 
@@ -35,12 +36,22 @@ for dataset in r.db('queryplayground').table('socrata_datasets').run():
         first_row = True
         for row in source_fp:
             if first_row:
-                row = row.replace(':', 'socrata_')
-                headers = row.split(',')
+                row = row.replace(':', 'socrata_').replace('@', '_')
+                headers = row.strip().split(',')
                 first_row = False
-            target_fp.write(row + newline)
-        schema = ','.join([col+':string' for col in headers])     
-        os.system('bq load fromsocrata.%s %s %s' % (dataset['id'], '2'+local_filename, schema))
-        os.system('rm 2%s; rm %s' % (local_filename, local_filename))
+            target_fp.write(row)
+        schema = []
+        for col in headers:
+            schema.append({"name": col.strip('"'), "type": "string", "mode": "nullable"})
+        #schema = ','.join([col.strip('"')+':string:nullable' for col in headers])
+        #print schema
+        import json
+        with open('schema.json', 'w') as f:
+            f.write(json.dumps(schema))
+        import json
+        cmd = 'bq load --apilog=- --schema=schema.json --skip_leading_rows=1 fromsocrata.%s %s' % (dataset['id']+'2', '2'+local_filename)
+        print cmd
+        os.system(cmd)
+        #os.system('rm 2%s; rm %s' % (local_filename, local_filename))
     #t = list(r.db('public').table('police_response_events').order_by(r.desc('socrata_created_at'), index=r.desc('socrata_created_at')).limit(int(1)).run())[0]['socrata_created_at'].isoformat()[:-9]
     #data = requests.get('https://data.seattle.gov/resource/pu5n-trf4.json?$select=:*,*&$limit=2000000&$where=:created_at%%20>%%20"%s"&$$app_token=%s' % (t, app_token)).json()
