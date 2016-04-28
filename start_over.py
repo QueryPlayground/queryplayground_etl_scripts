bcb670874a7bd29236be86568ef4eac3e2a80f62
Tim Clemans <tim@insideyourgovernment.com>
2016-04-28 23:45:46 +0000

    fix typo

--- start_over.py
+++ start_over.py
@@ -1,3 +1,4 @@
 import rethinkdb as r
-import os r.connect( "localhost", 28015).repl()
+import os
+r.connect( "localhost", 28015).repl()
 r.db('queryplayground').table('socrata_datasets').delete().run()
